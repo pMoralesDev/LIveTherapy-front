1e846a0cf3d499024dec6b626b1aa1abb9eb7aab
Pablo Morales <pmoralesdev@gmail.com>
2024-05-24 00:34:23 +0200

    Configuracion inicial

---
+++ src/service/authService.ts
@@ -0,0 +1,43 @@
+import axios from "axios";
+import axiosConfig from "../utils/config/axios.config";
+
+
+/**
+ * Funcion para inciar sesion
+ * @param {string} email email del usuario
+ * @param {string} password contrasena del usuario
+ * @returns peticion POST a la API
+ */
+export const login = (email:string, password:string) => {
+
+    // Declaramos el body para incluirlo en el POST
+    const body = {
+        email: email,
+        password: password,
+    }
+
+    // Enviamos la peticion POST al endpoint de login
+    return axiosConfig.post('auth/login', body)
+}
+
+/**
+ * Funcion para registrar un nuevo usuario
+ * @param {string} name Nombre del usuario
+ * @param {string} email Email del usuario
+ * @param {string} password Contrasena del usuario
+ * @param {number} age Edad del usuario
+ * @returns peticion POST a la API
+ */
+export const register = (name:string, email:string, password:string, age:number) => {
+
+    // Declaramos el body para incluirlo en el POST
+    const body = {
+        name,
+        email,
+        password,
+        age
+    }
+
+    // Enviamos la peticion POST al endpoint de login
+    return axiosConfig.post('auth/register', body)
+}
