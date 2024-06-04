88ae12e266010e2b6b605b34b33270f1bb5dd29a
Pablo Morales <pmoralesdev@gmail.com>
2024-06-04 15:13:16 +0200

    Visualizacion correcta de los informes de los terapeutas

---
+++ src/components/Terapeuta/Cards/InformesTerapeutaCard.tsx
@@ -0,0 +1,36 @@
+import { Card, CardContent, IconButton, Typography } from "@mui/material";
+import EditIcon from '@mui/icons-material/Edit';
+
+interface informesCardProps {
+    informe: any;
+}
+
+const InformesTerapeutaCard: React.FC<informesCardProps> = ({ informe }) => {
+
+    const dateObj = new Date(informe.fechaInforme);
+
+  const isValidDate = !isNaN(dateObj.getTime());
+  const formattedDate = isValidDate ? dateObj.toLocaleDateString('es-ES', {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+    weekday: 'long',
+    hour: '2-digit',
+    minute: '2-digit'
+  }) : "Fecha no válida";
+
+    return (
+        <Card style={{ marginBottom: 20 }}>
+            <CardContent>
+                <Typography variant="h5">{informe.paciente ? informe.paciente.nombre : 'Paciente no disponible'}</Typography>
+                <Typography color="textSecondary" >{formattedDate}</Typography>
+                <Typography color="textSecondary">{informe.informe}</Typography>
+                <IconButton>
+                    <EditIcon/>
+                </IconButton>
+            </CardContent>
+        </Card>
+    );
+};
+
+export default InformesTerapeutaCard;
--- src/components/Terapeuta/TerapeutaInformes.tsx
+++ src/components/Terapeuta/TerapeutaInformes.tsx
@@ -1,7 +1,36 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+import { AuthContext } from '../../utils/Interfaces/AuthInterface';
+import axiosConfig from '../../utils/config/axios.config';
+import InformesTerapeutaCard from './Cards/InformesTerapeutaCard';
 
 const TerapeutaInformes: React.FC = () => {
-  return <div>Informes Page</div>;
+
+  const [informes, setInformes] = useState<any[]>([]);
+  const { user: authUser } = useContext(AuthContext);
+
+  useEffect(() => {
+      if (authUser){
+        axiosConfig.get(`/terapias/citas/informes?id=${authUser.id}`)
+        .then(response => {
+          console.log(response.data);
+            setInformes(response.data);
+        })
+        .catch(error => {
+            console.error('Error fetching data:', error);
+        });
+      }
+      
+  }, [authUser]);
+
+  return (
+    <div>
+      <h1 style={{textAlign: 'center'}}>Tus Informes</h1>
+      {informes.map((informe, index) => (
+        <InformesTerapeutaCard key={`${informe.fechaInforme}-${index}`} informe={informe} />
+      ))}
+    </div>
+  );
+
 };
 
 export default TerapeutaInformes;
