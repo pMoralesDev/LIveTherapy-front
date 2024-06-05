846e3f395be4d0d6416d71b40419caf72588e8cd
Pablo Morales <pmoralesdev@gmail.com>
2024-06-05 10:02:34 +0200

    Se visualizan correctamente todos los datos en admin

--- src/components/Admin/Tables/AdminCuestionariosTable.tsx
+++ src/components/Admin/Tables/AdminCuestionariosTable.tsx
@@ -1,7 +1,68 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { ICuestionario } from '../../../utils/Interfaces/back/ICuestionario.interface';
+import axiosConfig from '../../../utils/config/axios.config';
+import { Box, Button, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@mui/material';
 
 export default function AdminCuestionariosTable() {
-  return (
-    <div>AdminCuestionariosTable</div>
-  )
-}
+  const [cuestionarios, setCuestionarios] = useState<ICuestionario[]>([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await axiosConfig.get('/cuestionarios'); 
+                setCuestionarios(response.data);
+            } catch (error) {
+                console.error('Error fetching data:', error);
+            }
+        };
+
+        fetchData();
+    }, []);
+
+    const handleOpenDialog = (cuestionario: ICuestionario) => {
+        // Lógica para abrir el diálogo de actualización
+    };
+
+    const handleDeleteCuestionario = (cuestionarioId: string) => {
+        // Lógica para eliminar el cuestionario
+    };
+
+    return (
+      <>
+        <TableContainer component={Paper}>
+            <Table>
+                <TableHead>
+                    <TableRow>
+                        <TableCell>Name</TableCell>
+                        <TableCell>Tipo</TableCell>
+                        <TableCell>Modelo</TableCell>
+                        <TableCell>Preguntas</TableCell>
+                        <TableCell>Respuestas</TableCell>
+                        <TableCell></TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {cuestionarios.map(cuestionario => (
+                        <TableRow key={cuestionario._id}>
+                            <TableCell>{cuestionario.name}</TableCell>
+                            <TableCell>{cuestionario.tipo}</TableCell>
+                            <TableCell>{cuestionario.modelo ? 'Sí' : 'No'}</TableCell>
+                            <TableCell>{cuestionario.preguntas?.length || 0}</TableCell>
+                            <TableCell>{cuestionario.respuestas?.length || 0}</TableCell>
+                            <TableCell>
+                                <div style={{ display: 'flex', alignItems: 'center' }}>
+                                    <Button variant="contained" color="primary" style={{ marginRight: '1rem' }} onClick={() => handleOpenDialog(cuestionario)}>Update</Button>
+                                    <Button variant="contained" color="secondary" onClick={() => { if (cuestionario._id) { handleDeleteCuestionario(cuestionario._id) } }}>Delete</Button>
+                                </div>
+                            </TableCell>
+                        </TableRow>
+                    ))}
+                </TableBody>
+            </Table>
+        </TableContainer>
+        <Box sx={{ display: 'flex', justifyContent: 'flex-end', marginTop: 2, marginRight: 2 }}>
+        <Button variant="contained" color="primary">Add Cuestionario</Button>
+        </Box>
+      </>
+    );
+};
--- src/components/Admin/Tables/AdminTerapiasTable.tsx
+++ src/components/Admin/Tables/AdminTerapiasTable.tsx
@@ -1,7 +1,72 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axiosConfig from '../../../utils/config/axios.config';
+import { ITerapia } from '../../../utils/Interfaces/back/ITerapia.interface';
+import { Box, Button, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@mui/material';
+import { IUser } from '../../../utils/Interfaces/back/IUser.interface';
 
 export default function AdminTerapiasTable() {
+  const [terapias, setTerapias] = useState<ITerapia[]>([]);
+
+  useEffect(() => {
+      const fetchData = async () => {
+          try {
+              const response = await axiosConfig.get('/terapias'); // Cambia la URL por la ruta de tu API
+              setTerapias(response.data);
+          } catch (error) {
+              console.error('Error fetching data:', error);
+          }
+      };
+
+      fetchData();
+  }, []);
+
+  const handleOpenDialog = (terapia: ITerapia) => {
+      // Lógica para abrir el diálogo de actualización
+  };
+
+  const handleDeleteTerapia = (terapiaId: string) => {
+      // Lógica para eliminar la terapia
+  };
+
   return (
-    <div>AdminTerapiasTable</div>
-  )
-}
+    <>
+      <TableContainer component={Paper}>
+          <Table>
+              <TableHead>
+                  <TableRow>
+                      <TableCell>Terapeuta</TableCell>
+                      <TableCell>Paciente</TableCell>
+                      <TableCell>Citas</TableCell>
+                      <TableCell>Registros</TableCell>
+                      <TableCell>Chat</TableCell>
+                      <TableCell></TableCell>
+                  </TableRow>
+              </TableHead>
+              <TableBody>
+                  {terapias.map(terapia => {
+                     const terapeuta : IUser = terapia.idTerapeuta as unknown as IUser
+                     const paciente : IUser = terapia.idPaciente as unknown as IUser
+                    return (
+                      <TableRow key={terapia._id}>
+                          <TableCell>{terapeuta.name}</TableCell>
+                          <TableCell>{paciente.name}</TableCell>
+                          <TableCell>{terapia.citas.length}</TableCell>
+                          <TableCell>{terapia.registros.length}</TableCell>
+                          <TableCell>{terapia.chat.length}</TableCell>
+                          <TableCell>
+                              <div style={{ display: 'flex', alignItems: 'center' }}>
+                                  <Button variant="contained" color="primary" style={{ marginRight: '1rem' }} onClick={() => handleOpenDialog(terapia)}>Update</Button>
+                                  <Button variant="contained" color="secondary" onClick={() => { if (terapia._id) { handleDeleteTerapia(terapia._id) } }}>Delete</Button>
+                              </div>
+                          </TableCell>
+                      </TableRow>
+                    )})}
+              </TableBody>
+          </Table>
+      </TableContainer>
+      <Box sx={{ display: 'flex', justifyContent: 'flex-end', marginTop: 2, marginRight: 2 }}>
+                <Button variant="contained" color="primary">Add Terapia</Button>
+      </Box>
+    </>
+  );
+};
